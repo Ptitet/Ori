f394712c581a60bfab9db8877fada9a98c92bd6c
Ptitet <ptitet73@gmail.com>
2024-06-28 14:58:06 +0200

    wip: added lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,11 @@ export default tseslint.config(
             }],
             '@typescript-eslint/restrict-template-expressions': ['error', {
                 allowNumber: true
+            }],
+            '@typescript-eslint/prefer-nullish-coalescing': ['error', {
+                ignorePrimitives: {
+                    boolean: true
+                }
             }]
         }
     },
